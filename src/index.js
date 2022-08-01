193d97d181065f876a519d6df4aa13ca581c2bd0
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-01 04:26:36 +0200

    Update index.js file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+console.log(
+    '+--------------+---------------------------------------------------------------+' + '\n' +
+    '| Project name | JavaScript Boilerplate                                        |' + '\n' +
+    '+--------------+---------------------------------------------------------------+' + '\n' +
+    '| Description  | Boilerplate for creating JavaScript projects. Includes basic  |' + '\n' + 
+    '|              | Webpack, Babel, Express, ESLint and Prettier configurations.  |' + '\n' +
+    '+--------------+---------------------------------------------------------------+' + '\n' +
+    '| Keywords     | Boilerplate, JavaScript, HTML, CSS, SASS, Webpack, Babel,     |' + '\n' + 
+    '|              | Node.js, Npm, Express, ESLint, Prettier                       |' + '\n' +
+    '+--------------+---------------------------------------------------------------+' + '\n' +
+    '| Version      | v1.0.0                                                        |' + '\n' +
+    '+--------------+---------------------------------------------------------------+'
+)
