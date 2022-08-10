f1dd0f35f67150f95547e1725526de58c785449e
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-10 18:21:28 +0200

    Add print-info.js file - logs informations about the project to the console

---
+++ src/app/print-info.js
@@ -0,0 +1,18 @@
+const printInfo = () => {
+    console.log(
+        '+--------------+---------------------------------------------------------------------------+\n' +
+        '| Project name | JavaScript Boilerplate                                                    |\n' +
+        '+--------------+---------------------------------------------------------------------------+\n' +
+        '| Description  | Boilerplate for creating projects in the JavaScript programming language. |\n' +
+        '|              | It includes the basic configuration of tools such as Webpack, Babel,      |\n' +
+        '|              | Jest, Express, ESLint and Prettier.                                       |\n' +
+        '+--------------+---------------------------------------------------------------------------+\n' +
+        '| Keywords     | Boilerplate, JavaScript, HTML, CSS, SASS, Webpack, Babel, Jest, Node.js,  |\n' +
+        '|              | Npm, Express, ESLint, Prettier                                            |\n' +
+        '+--------------+---------------------------------------------------------------------------+\n' +
+        '| Version      | v1.0.0                                                                    |\n' +
+        '+--------------+---------------------------------------------------------------------------+'
+    )
+}
+
+export { printInfo as default }
