93b19634b99a9f80c36256b6f35d9cbc6d606047
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-10 18:22:27 +0200

    Add render-app.js file - render app content

---
+++ src/app/render-app.js
@@ -0,0 +1,19 @@
+import logo from '../assets/images/index'
+
+const renderApp = () => {
+	const div = document.createElement('div')
+
+	div.className = 'container'
+	div.innerHTML = `
+		<img 
+			class="container__logo" 
+			src="${logo.size_256x256}" 
+			alt="JavaScript Boilerplate" 
+		/>
+		<h1 class="container__title">JavaScript Boilerplate</h1>
+    `
+
+	document.getElementById('root').appendChild(div)
+}
+
+export { renderApp as default }
