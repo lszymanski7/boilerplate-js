4dc31f67b5ebe0b38c620c25c07fa9869200be16
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-10 22:12:43 +0200

    Add constants.js file

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+// Logo - JavaScript
+
+const logoJS = {
+	size_128x128: require('../assets/images/logos/javascript-128x128.png'),
+	size_256x256: require('../assets/images/logos/javascript-256x256.png'),
+	size_512x512: require('../assets/images/logos/javascript-512x512.png')
+}
+
+export { logoJS as default }
