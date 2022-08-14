e63ab058a6fc4454074bbfc1d1529c07000fc5f9
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-14 05:17:09 +0200

    Format all files by Prettier

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,29 +1,22 @@
 module.exports = {
-	collectCoverageFrom: [
-		'<rootDir>/src/app/**'
-	],
-	coverageDirectory: '<rootDir>/coverage',
-	coverageReporters: [
-		'html', 
-		'text'
-	],
-	coverageThreshold: {
-		global: {
-			branches: 80,
-			functions: 80,
-			lines: 80,
-			statements: -10
-		}
-	},
-	rootDir: '../',
-	testEnvironment: 'jest-environment-jsdom',
-	testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-	testPathIgnorePatterns: [
-		'/node_modules/'
-	],
-	transform: {
-		'\\.[jt]sx?$': 'babel-jest',
-		'\\.(png|jpe?g|gif|svg|ico|webp|tiff|woff(2)?|eot|ttf|otf|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/utils/file-transformer.js'
-	},
-	verbose: true
+    collectCoverageFrom: ['<rootDir>/src/app/**'],
+    coverageDirectory: '<rootDir>/coverage',
+    coverageReporters: ['html', 'text'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10
+        }
+    },
+    rootDir: '../',
+    testEnvironment: 'jest-environment-jsdom',
+    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+    testPathIgnorePatterns: ['/node_modules/'],
+    transform: {
+        '\\.[jt]sx?$': 'babel-jest',
+        '\\.(png|jpe?g|gif|svg|ico|webp|tiff|woff(2)?|eot|ttf|otf|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/utils/file-transformer.js'
+    },
+    verbose: true
 }
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,59 +3,55 @@ const { merge } = require('webpack-merge')
 const path = require('path')
 
 module.exports = merge(common, {
-	mode: 'development',
-	devtool: 'inline-source-map',
-	stats: 'minimal',
-	output: {
-		filename: 'static/js/[name].js',
-		chunkFilename: 'static/js/[name].chunk.js',
-		path: path.resolve(__dirname, '../build')
-	},
-	module: {
-		rules: [
-			// CSS | SCSS
-			{
-				test: /\.(css|scss)$/,
-				use: [
-					'style-loader', 
-					'css-loader', 
-					'sass-loader'
-				]
-			},
-			// Fonts
-			{
-				test: /\.(woff(2)?|eot|ttf|otf)$/,
-				type: 'asset/inline',
-				generator: {
-					filename: 'static/fonts/[name][ext]'
-				}
-			},
-			// Images
-			{
-				test: /\.(png|jpe?g|gif|svg|ico|webp|tiff)$/,
-				type: 'asset/resource',
-				generator: {
-					filename: 'static/images/[name][ext]'
-				}
-			},
-			// Media
-			{
-				test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)$/,
-				type: 'asset/resource',
-				generator: {
-					filename: 'static/media/[name][ext]'
-				}
-			}
-		]
-	},
-	devServer: {
-		client: {
-			logging: 'warn'
-		},
-		compress: true,
-		historyApiFallback: true,
-		hot: true,
-		open: true,
-		port: 8080
-	}
+    mode: 'development',
+    devtool: 'inline-source-map',
+    stats: 'minimal',
+    output: {
+        filename: 'static/js/[name].js',
+        chunkFilename: 'static/js/[name].chunk.js',
+        path: path.resolve(__dirname, '../build')
+    },
+    module: {
+        rules: [
+            // CSS | SCSS
+            {
+                test: /\.(css|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            // Fonts
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/inline',
+                generator: {
+                    filename: 'static/fonts/[name][ext]'
+                }
+            },
+            // Images
+            {
+                test: /\.(png|jpe?g|gif|svg|ico|webp|tiff)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'static/images/[name][ext]'
+                }
+            },
+            // Media
+            {
+                test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'static/media/[name][ext]'
+                }
+            }
+        ]
+    },
+    devServer: {
+        client: {
+            logging: 'warn'
+        },
+        compress: true,
+        historyApiFallback: true,
+        hot: true,
+        open: true,
+        port: 8080
+    }
 })
