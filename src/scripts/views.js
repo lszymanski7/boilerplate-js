cc4c168cb50ecca816f341f5240230f96af70005
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-29 17:22:41 +0200

    Update project structure

---
+++ src/scripts/views.js
@@ -0,0 +1,16 @@
+import logo from '../data/constants'
+
+const render = () => {
+    document.getElementById('home-root').innerHTML = `
+        <div class="home-container">
+            <img
+                class="home-logo"
+                src="${logo.size_256x256}"
+                alt="JavaScript Logo"
+            />
+            <h1 class="home-title">JavaScript Boilerplate</h1>
+        </div>
+    `
+}
+
+export { render as default }
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, '../src/index.js')
+        main: path.resolve(__dirname, '../src/scripts/index.js')
     },
     output: {
         path: path.resolve(__dirname, '../build'),
