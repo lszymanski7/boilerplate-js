d40eb2a1cf92676fd76e751e6b8b461289479f72
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2023-06-08 19:34:29 +0200

    Update indentation from 4 to 2 spaces

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -1,29 +1,36 @@
 module.exports = {
-    collectCoverageFrom: [
-        '<rootDir>/src/scripts/**/*.js',
-        '<rootDir>/src/utils/**/*.js'
-    ],
-    coverageDirectory: '<rootDir>/coverage',
-    coveragePathIgnorePatterns: [
-        '/node_modules/', 
-        '/__tests__/'
-    ],
-    coverageReporters: ['html', 'text'],
-    coverageThreshold: {
-        global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: -10
-        }
-    },
-    rootDir: '../../',
-    testEnvironment: 'jest-environment-jsdom',
-    testMatch: ['**/__tests__/**/*.(spec|test).js'],
-    testPathIgnorePatterns: ['/node_modules/'],
-    transform: {
-        '\\.js$': 'babel-jest',
-        '\\.(png|jpe?g|gif|svg|ico|webp|tiff|woff(2)?|eot|ttf|otf|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/jest/file-transformer.js'
-    },
-    verbose: true
+  collectCoverageFrom: [
+    '<rootDir>/src/scripts/**/*.js', 
+    '<rootDir>/src/utils/**/*.js'
+  ],
+  coverageDirectory: '<rootDir>/coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/', 
+    '/__tests__/'
+  ],
+  coverageReporters: [
+    'html', 
+    'text'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  },
+  rootDir: '../../',
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: [
+    '**/__tests__/**/*.(spec|test).js'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  transform: {
+    '\\.js$': 'babel-jest',
+    '\\.(png|jpe?g|gif|svg|ico|webp|tiff|woff(2)?|eot|ttf|otf|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/jest/file-transformer.js'
+  },
+  verbose: true
 }
--- src/scripts/render.js
+++ src/scripts/render.js
@@ -4,64 +4,64 @@ import { BADGES, LOGOS, SHAPES } from '../data/constants'
 
 // Function ➞ renders the application
 const render = () => {
-    // Application name
-    const name = 'JavaScript Boilerplate'
+  // Application name
+  const name = 'JavaScript Boilerplate'
 
-    // Application HTML code
-    const html = `
-        <div class="fullscreen">
-            <header class="header">
-                <img
-                    alt="Waves"
-                    src="${SHAPES.waves}"
-                />
-            </header>
-            <div class="home">
-                <img
-                    alt="Application Logo"
-                    class="home__app-logo bounce"
-                    src="${LOGOS.javascript.svg}"
-                />
-                <h1 class="home__app-name">${name}</h1>
-                <div class="home__badges">
-                    <a href="https://github.com/lszymanski7/boilerplate-js">
-                        <img
-                            alt="GitHub Badge"
-                            src="${BADGES.github}"
-                        />
-                    </a>
-                    <img
-                        alt="Version Badge"
-                        src="${BADGES.version}"
-                    />
-                    <img
-                        alt="Size Badge"
-                        src="${BADGES.size}"
-                    />
-                    <a href="https://github.com/lszymanski7/boilerplate-js/blob/main/LICENSE.md">
-                        <img
-                            alt="License Badge"
-                            src="${BADGES.license}"
-                        />
-                    </a>
-                    <img
-                        alt="Forks Badge"
-                        src="${BADGES.forks}"
-                    />
-                    <img
-                        alt="Stars Badge"
-                        src="${BADGES.stars}"
-                    />
-                </div>
-            </div>
-            <footer class="footer">
-                <p>Created with &#x1F49B; by Łukasz Szymański</p>
-            </footer>
+  // Application HTML code
+  const html = `
+    <div class="fullscreen">
+      <header class="header">
+        <img
+          alt="Waves"
+          src="${SHAPES.waves}"
+        />
+      </header>
+      <div class="home">
+        <img
+          alt="Application Logo"
+          class="home__app-logo bounce"
+          src="${LOGOS.javascript.svg}"
+        />
+        <h1 class="home__app-name">${name}</h1>
+        <div class="home__badges">
+          <a href="https://github.com/lszymanski7/boilerplate-js">
+            <img
+              alt="GitHub Badge"
+              src="${BADGES.github}"
+            />
+          </a>
+          <img
+            alt="Version Badge"
+            src="${BADGES.version}"
+          />
+          <img
+            alt="Size Badge"
+            src="${BADGES.size}"
+          />
+          <a href="https://github.com/lszymanski7/boilerplate-js/blob/main/LICENSE.md">
+            <img
+              alt="License Badge"
+              src="${BADGES.license}"
+            />
+          </a>
+          <img
+            alt="Forks Badge"
+            src="${BADGES.forks}"
+          />
+          <img
+            alt="Stars Badge"
+            src="${BADGES.stars}"
+          />
         </div>
-    `
+      </div>
+      <footer class="footer">
+        <p>Created with &#x1F49B; by Łukasz Szymański</p>
+      </footer>
+    </div>
+  `
 
-    // Element method ➞ inserts HTML code into a specified position
-    document.getElementById('root').insertAdjacentHTML('afterbegin', html)
+  // Element method ➞ inserts HTML code into a specified position
+  document.getElementById('root').insertAdjacentHTML('afterbegin', html)
 }
 
 export default render
