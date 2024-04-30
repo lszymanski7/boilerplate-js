4792013e470eebe4f4968c3cd23ec74d46f34664
Lukasz Szymanski <32853544+lszymanski7@users.noreply.github.com>
2024-04-30 18:49:12 +0200

    Update jest.config.js file

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -17,7 +17,8 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/'],
   transform: {
     '\\.js$': 'babel-jest',
-    '\\.(png|jpe?g|gif|svg|ico|webp|tiff|woff(2)?|eot|ttf|otf|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/jest/file-transformer.js'
+    '\\.(png|jpe?g|gif|svg|ico|webp|tiff|woff(2)?|eot|ttf|otf|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/config/jest/file-transformer.js'
   },
   verbose: true
 }
